295f1bdce7753ecbfc8eaa56f58352d3410503a8
goqkTk <jty_419@naver.com>
2025-05-13 14:50:13 +0900

    Revert "DB host localhost로 변경"
    This reverts commit 99cf262b6e5a5f9e065021c4dadd523a640b3c89.

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ function escapeHtml(unsafe) {
 
 // MySQL 연결 설정
 const db = mysql.createConnection({
-    host: 'localhost',
+    host: '0.0.0.0',
     user: 'root',
     password: '1234',
     database: 'infinitetuk'
